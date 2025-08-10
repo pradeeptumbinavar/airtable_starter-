cef778d2daa5134c5212b79bbfa037a1c9697f42
pradeeptumbinavar <pradeeptumbinavar@gmail.com>
2025-08-10 11:50:14 +0530

    add env and access token

--- cmd/airtable-client/main.go
+++ cmd/airtable-client/main.go
@@ -1,33 +1,53 @@
 package main
 
 import (
+	"airtable_starter/gen/airtableapi"
+	"airtable_starter/pkg/httpclient"
 	"context"
 	"fmt"
 	"log"
+	"os"
 
-	"airtable_starter/gen/airtableapi"
-	"airtable_starter/pkg/httpclient"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	token := "YOUR_AIRTABLE_TOKEN"
-	client := httpclient.NewBearerClient(token)
-
-	airtableClient, err := airtableapi.NewClientWithResponses("https://api.airtable.com/v0", airtableapi.WithHTTPClient(client))
+	// Load .env file
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file")
 	}
 
-	resp, err := airtableClient.GetRecordWithResponse(
-		context.Background(),
-		"baseId",
-		"tableName",
-		"recordId",
-		&airtableapi.GetRecordParams{}, // provide empty params or set fields as needed
+	// Read variables from environment
+	token := os.Getenv("AIRTABLE_TOKEN")
+	baseID := os.Getenv("AIRTABLE_BASE_ID")
+	tableID := os.Getenv("AIRTABLE_TABLE_ID")
+	recordID := os.Getenv("AIRTABLE_RECORD_ID_2") // Use the first record ID for this example
+
+	// Create HTTP client with Bearer token
+	client := httpclient.NewBearerClient(token)
+
+	// Create Airtable API client
+	airtableClient, err := airtableapi.NewClientWithResponses(
+		"https://api.airtable.com/v0",
+		airtableapi.WithHTTPClient(client),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Call the API to get a record
+	resp, err := airtableClient.GetRecordWithResponse(
+		context.Background(),
+		baseID,
+		tableID,
+		recordID,
+		&airtableapi.GetRecordParams{}, // keep empty for now
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print raw response body
 	fmt.Println(string(resp.Body))
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module airtable_starter
 go 1.24.4
 
 require (
-	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/go-chi/chi/v5 v5.2.2
-	github.com/google/uuid v1.5.0 // indirect
 	github.com/oapi-codegen/runtime v1.1.2
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
